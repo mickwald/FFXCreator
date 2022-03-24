2d0d0c5ca7e042fe585c79257d4966746dcdb22a
mickwald <mickwald@hotmail.com>
2022-03-24 15:37:44 +0100

    Moved textureupdate to OnValidate and added scrolling effects. Moved scrolling to Selected function

--- Assets/TextureManager.cs
+++ Assets/TextureManager.cs
@@ -1,39 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TextureManager : MonoBehaviour
 {
+    public bool EditorScroll;
+
+
     private Material material;
+    public Material textureMaterial;
     public Texture2D baseTex;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ApplyTexture();
-    }
+    
 
-    // Update is called once per frame
+    public float scrollX = .5f;
+    public float scrollY = .5f;
+
+    private float offsetX = .5f;
+    private float offsetY = .5f;
+
+
     void Update()
     {
+        offsetX = scrollX * Time.time;
+        offsetY = scrollY * Time.time;
+        textureMaterial.mainTextureOffset = new Vector2(offsetX, offsetY);
 
     }
 
-    private void OnDrawGizmos()
+    private void OnValidate()
     {
-        if (baseTex == null)
+
+        if (baseTex == null || textureMaterial == null)
         {
             return;
         }
-        //ApplyTexture();
-        //this.GetComponent<Renderer>().sharedMaterial;
+
+        ApplyTexture();
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (EditorScroll) { ScrollTexture(); }
+        textureMaterial.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+
+    private void ScrollTexture()
+    {
+        offsetX = scrollX * Time.realtimeSinceStartup;
+        offsetY = scrollY * Time.realtimeSinceStartup;
+
     }
 
     private void ApplyTexture()
     {
-        Material temp = new Material(this.gameObject.GetComponent<Renderer>().material);
-        temp.mainTexture = baseTex;
+        textureMaterial.mainTexture = baseTex;
 
-        this.GetComponent<Renderer>().material = temp;
     }
 }
